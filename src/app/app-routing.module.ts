ed6b51f75b8f1f29bf2be8ad53b496d1a26bf5ac
Alexandre1313 <alexandre.13a@gmail.com>
2022-03-19 22:24:55 -0300

    html e css componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule, Component } from '@angular/core';
@@ -5,6 +6,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
