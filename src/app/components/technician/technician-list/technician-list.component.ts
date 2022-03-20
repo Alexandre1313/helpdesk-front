418df1f39dd82df7efefd243b56ccbf1cb902bc8
Alexandre1313 <alexandre.13a@gmail.com>
2022-03-20 19:53:07 -0300

    finalizando tecnicos list, falta implementar botão

--- src/app/components/technician/technician-list/technician-list.component.ts
+++ src/app/components/technician/technician-list/technician-list.component.ts
@@ -31,4 +31,9 @@ export class TechnicianListComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
     });
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
