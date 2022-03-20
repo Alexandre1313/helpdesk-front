626ed217f2a2ea85a80f832f442a93e638169d93
Alexandre1313 <alexandre.13a@gmail.com>
2022-03-20 19:56:42 -0300

    criando componente technician-create

---
+++ src/app/components/technician/create-technician/create-technician.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-technician',
+  templateUrl: './create-technician.component.html',
+  styleUrls: ['./create-technician.component.css']
+})
+export class CreateTechnicianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
